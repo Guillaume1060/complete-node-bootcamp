0d66f64992f4c0dcdeb27cb3cbed94e3cf626f14
guillaume1060 <breyerguillaume@gmail.com>
2022-10-04 17:16:09 +0200

    update Queries

--- 4-natours/starter/dev-data/controllers/tourController.js
+++ 4-natours/starter/dev-data/controllers/tourController.js
@@ -2,9 +2,40 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTour = async (req, res) => {
     try {
-        console.log(req.query);
+        // BUILD A QUERY
+        // 1A) Filtering
+        const queryObject = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObject[el]);
 
-        const tours = await Tour.find();
+        // 1B) Advanced filtering
+        let queryStr = JSON.stringify(queryObject);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        // { difficulty: 'easy' , duration: { $gte: 5 } }   => mongoose requete necessaire
+        // { difficulty: 'easy', 'duration[gte}': '5' }     => console.log(req.query);
+        // donc ci dessus on a ajouté le $ à queryStr
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+            // sort('price Rating')
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // const query = Tour.find()
+        // .where('duration')
+        // .equals(5)
+        // .where('difficulty')
+        // .equals('easy');
+
+        // SEND RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
