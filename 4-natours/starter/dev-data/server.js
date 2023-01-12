95a7f1658c17f94124e3117e5eb9c7c94d3ba232
Guillaume <breyerguillaume@gmail.com>
2023-01-12 19:50:14 +0100

    restart lesson

--- 4-natours/starter/dev-data/server.js
+++ 4-natours/starter/dev-data/server.js
@@ -5,24 +5,21 @@ const app = require('./app');
 dotenv.config({ path: '../config.env' });
 
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    // .connect(process.env.DATABASE_LOCAL, {  /// LOCAL CONNECTION
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection successful!'));
-
+  // .connect(process.env.DATABASE_LOCAL, {  /// LOCAL CONNECTION
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
-
-
